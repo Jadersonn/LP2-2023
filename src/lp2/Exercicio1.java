e5a8e651c4526a46571702654d149925284d17e0
Sidowe102 <jaderson6100@gmail.com>
2023-02-27 21:07:36 -0400

    primeira versao

---
+++ src/lp2/Exercicio1.java
@@ -0,0 +1,14 @@
+package lp2;
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) {
+		Scanner leia = new Scanner (System.in);
+		System.out.println("Informe um nome: ");
+		String nome = leia.nextLine();
+		System.out.println("Seu nome eh " + nome);
+
+	}
+
+}
